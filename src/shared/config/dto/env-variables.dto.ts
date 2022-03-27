37e147e65261cfb5e61eea59af2a1d395e45f6a4
Full-Stack-Shokhboz-Abdullaev <shokhboz11abdullayev@gmail.com>
2022-03-27 12:11:39 +0500

    deploy env

--- src/shared/config/dto/env-variables.dto.ts
+++ src/shared/config/dto/env-variables.dto.ts
@@ -1,12 +1,14 @@
-import { IsEnum, IsNumber, Matches } from 'class-validator';
+import { IsEnum, IsNumber, IsOptional, Matches } from 'class-validator';
 import { MONGO_URI_PATTERN } from '../constants/regexs';
 import { Environment } from '../typings/enums/env.enum';
 
 export class EnvironmentVariables {
   @IsEnum(Environment)
+  @IsOptional()
   NODE_ENV: Environment;
 
   @IsNumber()
+  @IsOptional()
   PORT: number;
 
   @Matches(MONGO_URI_PATTERN)
